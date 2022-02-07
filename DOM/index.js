5d0cde5578ae01c5fd7e86c160a116207d5b6902
Binary <binarakuanysheva13@gmail.com>
2022-02-07 22:12:29 +0600

    Transfering to GitHub

---
+++ DOM/index.js
@@ -0,0 +1,10 @@
+// .dropdown .dropdown-menu {
+//   -webkit-transition: all .2s linear;
+//   -o-transition: all .2s linear;
+//   transition: all .2s linear;
+//   display: block !important;
+//   transform: scale(0);
+// }
+// .dropdown .dropdown-menu.show{
+//   transform: scale(1);
+// }
